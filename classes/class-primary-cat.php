425d999f8b047f8aa1cd99735c0c804377247d19
mirkoviviano <info@mirkoviviano.it>
2021-08-16 19:59:56 +0100

    Class::PrimaryCategory: finished comments. Finished save function. Refactoring.

--- classes/class-primary-cat.php
+++ classes/class-primary-cat.php
@@ -1,16 +1,23 @@
 <?php
+/**
+ * Primary Category class: the constructor doesn't take any parameters.
+ * The constructor adds actions to the hooks "add_meta_boxes" and "save_post":
+ * the former adds a metabox to the post page whilst the latter adds/updates
+ * a metadata value to the post's metadata set.
+ */
 namespace MKR\MVPrimaryCategory;
 
 class PrimaryCategory{
 	/**
 	 * PrimaryCategory constructor.
 	 * Simply adds action to WordPress.
-	 * "Actions are the hooks that the WordPress core launches at specific points during execution, or when specific events occur. Plugins can specify that one or more of its PHP functions are executed at these points, using the Action API."
+	 * Actions are the hooks that the WordPress core launches at specific points during execution, or when specific events occur. Plugins can specify that one or more of its PHP functions are executed at these points, using the Action API.
 	 * @link https://developer.wordpress.org/reference/functions/add_action/
 	 * 
 	 */
 	public function __construct() {
 		add_action('add_meta_boxes', array( $this, 'addPrimaryCategoryBox' ));
+		add_action('save_post', array( $this, 'savePrimaryCategory' ));
 	}
 
 	/**
@@ -21,17 +28,29 @@ class PrimaryCategory{
 	 * When a user edits a post, the edit screen is composed of several default boxes: Editor, Publish, Categories, Tags, etc. These boxes are meta boxes. Plugins can add custom meta boxes to an edit screen of any post type.
 	 * The content of custom meta boxes are usually HTML form elements where the user enters data related to a Plugin’s purpose, but the content can be practically any HTML you desire.
 	 * @link https://developer.wordpress.org/plugins/metadata/custom-meta-boxes/
+	 * 
+	 * @return void
 	 */
 	public function addPrimaryCategoryBox() {
 		$allPostTypes = get_post_types();
 
+		/** 
+		 * @param string 					$id				(Required) Meta box ID (used in the 'id' attribute for the meta box).
+		 * @param string 					$title			(Required) Title of the meta box.
+		 * @param callable 					$callback 		(Required) Function that fills the box with the desired content. The function should echo its output.
+		 * @param string|array|WP_Screen 	$screen			(Optional) The screen or screens on which to show the box.
+		 * @param string					$context		(Optional) The context within the screen where the box should display. 
+		 * @param string 					$priority		(Optional) The priority within the context where the box should show.
+		 * 
+		 * @link https://developer.wordpress.org/reference/functions/add_meta_box/
+		*/
 		add_meta_box(
-			'primary_category_box',
-			'Primary Category',
-			array( $this, 'populatePrimaryCategoryBox' ),
-			$allPostTypes,
-			'side',
-			'high'
+			'primary_category_box', 						
+			'Primary Category', 							
+			array( $this, 'populatePrimaryCategoryBox' ), 	
+			$allPostTypes, 									
+			'side',											
+			'high'											
 		);
 	}
 
@@ -69,8 +88,18 @@ class PrimaryCategory{
     }
 
 	/**
-	 * TODO: Save primary category
+	 * Save primary category as post's metadata using WordPress update_post_meta() function.
+	 * 
+	 * @return void
 	*/
-	
+	public function savePrimaryCategory(){
+		global $post;
+
+		if ( isset($_POST['primaryCategory']) ) {
+			$primaryCategory = sanitize_text_field( $_POST[ 'primaryCategory' ] );
+			update_post_meta( $post->ID, 'primaryCategory', $primaryCategory );
+		}
+	}
+
 
 }
